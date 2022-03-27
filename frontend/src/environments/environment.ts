a31397f45746e52cba5589d1444e24dde6c57564
omarqr0 <pleess30@gmail.com>
2022-03-27 15:43:53 +0300

    implemented get_token_auth_header

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,16 @@
-/* @TODO replace with your variables
+import credentials from 'src/environments/.cred'
+/* TODO replace with your variables
  * ensure all variables on this page match your project
  */
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  //TODO setup auth0 env
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'http://127.0.0.1:5000', // the auth0 domain prefix
+    audience: credentials.audience, // the audience set for the auth0 app
+    clientId: credentials.id, // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
